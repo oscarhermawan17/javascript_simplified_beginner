32de3b130903042f3bcde142043d1f49860f6a0a
unknown <oscar.hermawan90@gmail.com>
2023-05-21 21:32:59 +0800

    section-10: DatePicker done

---
+++ 10_ModulesOrBundlers/69_ES6Modules/script.js
@@ -0,0 +1,6 @@
+import printUser, { oscar, fiska } from "./user.js"
+
+console.log(oscar)
+console.log(fiska)
+
+printUser(oscar)
---
+++ 10_ModulesOrBundlers/69_ES6Modules/user.js
@@ -0,0 +1,13 @@
+export const oscar = {
+  name: "Oscar",
+  age: 25,
+}
+
+export const fiska = {
+  name: "Fiska",
+  age: 25,
+}
+
+export default function printUser(user) {
+  console.log(`Name: ${user.name}. Age: ${user.age}`)
+}
